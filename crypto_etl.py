1e997d1880544b8992863e4348f014f7a3341426
nathalier <nathalier@gmail.com>
2022-11-11 15:53:49 +0000

    Open db connection and insert crypto currencies list to db

--- crypto_etl.py
+++ crypto_etl.py
@@ -1,5 +1,7 @@
 import json
+import pandas as pd
 import requests
+import sqlite3
 
 
 API_URL_BASE = 'https://api.coingecko.com/api/v3/'
@@ -14,6 +16,32 @@ def get_coin_list():
     return json.loads(response.content)
 
 
+def add_coins_to_db(crypto_currencies, conn):
+    coins_df = pd.DataFrame(crypto_currencies)
+    coins_df.set_index('id')
+    coins_df.to_sql(name='crypto_cur', con=conn, if_exists='replace', method='multi')
+
+
+def prepare_db(conn):
+    conn.executescript('''
+            CREATE TABLE IF NOT EXISTS crypto_cur (
+                id	TEXT NOT NULL PRIMARY KEY UNIQUE,
+                symbol TEXT NOT NULL,
+                name TEXT NOT NULL );
+            ''')
+    conn.commit()
+
+
+def connect_to_db(db_name='crypto_market.sqlite'):
+    conn = sqlite3.connect(db_name)
+    prepare_db(conn)
+    return conn
+
+
 if __name__ == '__main__':
+    conn = connect_to_db()
+
     coins_list = get_coin_list()
+    add_coins_to_db(coins_list, conn)
+
     print(len(coins_list))
